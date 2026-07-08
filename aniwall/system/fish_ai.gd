8d518dd0119e3b1c06e04a542d77294a941bb9de
Rendo <Rendo>
2026-07-09 00:29:22 +0500

    Gore

---
+++ aniwall/system/fish_ai.gd
@@ -0,0 +1,37 @@
+extends Node3D
+
+var rng: RandomNumberGenerator
+
+@onready var destination: Vector3 = global_position
+@export var animator: AnimationPlayer
+var timer: Timer
+
+func _ready() -> void:
+	rng = RandomNumberGenerator.new()
+	rng.randomize()
+	timer = Timer.new()
+	add_child(timer)
+	update_destination()
+	timer.timeout.connect(update_destination)
+
+func _process(delta: float) -> void:
+	global_position = lerp(global_position,destination,delta)
+	if global_position != destination:
+		var current_quat: Quaternion = global_transform.basis.get_rotation_quaternion()
+	
+		var target_basis: Basis = global_transform.looking_at(
+			destination, Vector3.UP
+		).basis
+		var target_quat: Quaternion = target_basis.get_rotation_quaternion()
+		
+		# Ensure shortest path (avoids weird flips)
+		if current_quat.dot(target_quat) < 0.0:
+			target_quat = -target_quat
+		
+		var result_quat = current_quat.slerp(target_quat, delta)
+		global_transform.basis = Basis(result_quat)
+	animator.speed_scale = global_position.distance_squared_to(destination)
+
+func update_destination() -> void:
+	destination = Vector3(rng.randf_range(-1,1),rng.randf_range(0.5,3),rng.randf_range(-4,4))
+	timer.start(rng.randf_range(3,5))
--- aniwall/system/qr_code.gd
+++ aniwall/system/qr_code.gd
@@ -3,5 +3,12 @@ extends QRCodeRect
 
 
 func _ready() -> void:
-	var ip = Array(IP.get_local_addresses()).filter(func(a): return a.begins_with("192.168.") or a.begins_with("10.") or a.begins_with("172.")).front()
+	generate()
+
+func generate() -> void:
+	var ip = Array(IP.get_local_addresses()).filter(func(a): return a.begins_with("192.168.") or a.begins_with("10.") or a.begins_with("172.")).pick_random()
 	set_data(ip)
+
+func _gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseButton and event.is_pressed():
+		generate()
